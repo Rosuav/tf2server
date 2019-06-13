2a2b16f937129cafb3de903f376f47cefa2bd893
Chris Angelico <rosuav@gmail.com>
2019-06-13 17:57:36 +1000

    Show duplicate line count

--- ramusage.py
+++ ramusage.py
@@ -3,7 +3,7 @@ import sys
 import time
 pid = 29182
 print("%9s %8s %8s %8s %8s %8s" % ("time", "size", "rss", "shared", "text", "data"))
-lastinfo = None
+lastinfo, count = None, 0
 while True:
 	try: exe = os.readlink("/proc/%s/exe" % pid)
 	except PermissionError: exe = ""
@@ -15,6 +15,9 @@ while True:
 		size, rss, shared, text, _, data, _ = f.read().strip().split()
 	info = "%8s %8s %8s %8s %8s" % (size, rss, shared, text, data)
 	if info != lastinfo:
-		lastinfo = info
+		if count > 10: print("(+%d)" % count)
+		lastinfo, count = info, 0
 		print("%9s %s" % (time.strftime("%a %H:%M"), info))
+	else:
+		count += 1
 	time.sleep(1800)
